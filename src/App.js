ce9f511f846af19758be67e0a2bc87b31e43b61f
mixelpixel <mixelpix@mac.com>
2017-07-27 01:23:00 -0400

    importing MovieDetail into App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import Movies from './Movies';
+import MovieDetail from './MovieDetail';
 import './App.css';
 
 class App extends Component {
