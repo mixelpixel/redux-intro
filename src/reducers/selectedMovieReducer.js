2a18ad2d045122cf2c3937bf6b6b9097e958f38b
mixelpixel <mixelpix@mac.com>
2017-07-27 01:12:49 -0400

    idem

--- src/reducers/selectedMovieReducer.js
+++ src/reducers/selectedMovieReducer.js
@@ -2,7 +2,7 @@ export default (selectedMovie = null, action) => {
   switch(action.type) {
     case 'MOVIE_SELECTED':
       return action.payload;
-    default
+    default:
       return selectedMovie;
   }
 };
