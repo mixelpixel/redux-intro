aa898878d33cc29a2f5afd141c7e50c94bc373dc
mixelpixel <mixelpix@mac.com>
2017-07-27 01:11:45 -0400

    switch statement

---
+++ src/reducers/selectedMovieReducer.js
@@ -0,0 +1,8 @@
+export default (selectedMovie = null, action) => {
+  switch(action.type) {
+    case 'MOVIE_SELECTED':
+      return action.payload;
+    default
+      return selectedMovie;
+  }
+};
