c3c1d8c72c05aec778bdb88ac092be496de44e4c
mixelpixel <mixelpix@mac.com>
2017-07-26 23:30:10 -0400

    what the program will handle

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers} from 'redux';
 
+// to accomodate an array of movie titles
+// and the selection of one movie
 const rootReducer = combineReducers({
-
+  movies: 
 });
 
 export rootReducer;
