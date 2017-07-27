1d8b6a2e8233a9d8e936814b365a857a09005d4c
mixelpixel <mixelpix@mac.com>
2017-07-27 01:09:04 -0400

    selectedMovieReducer import statement and file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers} from 'redux';
 import moviesReducer from './movies';
+import selectedMovieReducer from './selectedMovieReducer';
 
 // to accomodate an array of movie titles
 // and the selection of one movie
 const rootReducer = combineReducers({
-  movies: moviesReducer
+  movies: moviesReducer,
+  selectedMovies: selectedMovieReducer
 });
 
 export default rootReducer;
