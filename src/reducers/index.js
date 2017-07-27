f69a2168611da7302e0a03e22b4c6b8b70f9ab8f
mixelpixel <mixelpix@mac.com>
2017-07-26 23:32:12 -0400

    import statement for files who's function is to return an array of movie titles

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers} from 'redux';
+import MoviesReducer from './movies';
 
 // to accomodate an array of movie titles
 // and the selection of one movie
 const rootReducer = combineReducers({
-  movies: 
+  movies:
 });
 
 export rootReducer;
