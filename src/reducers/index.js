a4ded443a48fb50d01a87e7ef49e8e95cf9357bd
mixelpixel <mixelpix@mac.com>
2017-07-26 23:36:11 -0400

    camelCase

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers} from 'redux';
-import MoviesReducer from './movies';
+import moviesReducer from './movies';
 
 // to accomodate an array of movie titles
 // and the selection of one movie
