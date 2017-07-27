972dddef35ac14e30065e7e8c092762503fb3788
mixelpixel <mixelpix@mac.com>
2017-07-27 02:54:41 -0400

    console.log demo of reducers

--- src/reducers/selectedMovieReducer.js
+++ src/reducers/selectedMovieReducer.js
@@ -1,4 +1,5 @@
 export default (selectedMovie = null, action) => {
+  console.log('>>>>>>>>> selectedMovieReducer', action);
   switch(action.type) {
     case 'MOVIE_SELECTED':
       return action.payload;
