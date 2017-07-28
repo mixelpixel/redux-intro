7170ef456539acff5e3ae7b49a7174c95071e9a1
mixelpixel <mixelpix@mac.com>
2017-07-27 21:09:47 -0400

    ...

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import reducers from './reducers'
 
 ReactDOM.render(
   <Provider store={createStore(reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )}>
     <App />
   </Provider>,
   document.getElementById('root'));
